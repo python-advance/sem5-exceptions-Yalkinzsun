cca2ee58e4946b7b7348c952438a54be3bea6e57
Kirill Skorobogatov <Kirill9806@gmail.com>
2018-12-07 16:27:00 +0300

    Update main.py

--- Инвариантная самостоятельная работа/main.py
+++ Инвариантная самостоятельная работа/main.py
@@ -1,4 +1,5 @@
 import json
+from prettytable import PrettyTable
 
 def FromJsonToTable(filename):
   try:
@@ -13,7 +14,7 @@ def FromJsonToTable(filename):
   #использование менеджера контекста
   with open(filename,'r') as data_file:    
      data = json.load(data_file)
-
+  
   name, surname, sex, birth, nationality = [],[],[],[],[]
   for i in range(len(data["fields"])):
     name.append(data["fields"][i]["name"])
@@ -21,19 +22,33 @@ def FromJsonToTable(filename):
     sex.append(data["fields"][i]["sex"])
     birth.append(data["fields"][i]["date of birth"])
     nationality.append(data["fields"][i]["nationality"])
-
+    
+  # формирование и вывод таблицы без использования стронних библиотек
   titles = ['name', 'surname', 'sex', 'birth', 'nationality']
   dt = [titles] + list(zip(name, surname, sex, birth, nationality))
   try:
     iter(dt)
   except TypeError:
-    print(" Не итеребл")
-
+    print(" Не iterable-объект!")
+    
+  """
   for i, d in enumerate(dt):
       line = ' '.join(str(x).ljust(14) for x in d)
       print(line)
       if i == 0:
-          print('-' * len(line))
+         print('-' * len(line))
+  """
+  
+  #использование библиотеки PrettyTable для формирования таблицы      
+  x = PrettyTable()
+  column_names = ['name', 'surname', 'sex', 'birth', 'nationality']
+  x.add_column(column_names[0], name)
+  x.add_column(column_names[1], surname)
+  x.add_column(column_names[2], sex)
+  x.add_column(column_names[3], birth)
+  x.add_column(column_names[4], nationality)
+  print(x)
+             
   return len(data["fields"]) 
 
 if __name__ == "__main__":
